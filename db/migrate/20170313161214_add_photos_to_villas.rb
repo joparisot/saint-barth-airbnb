62c721a3c5f69ae226b6e78f264de5ad00779be0
jonathan parisot <parisot.jonathan@gmail.com>
2017-03-13 17:45:04 +0100

    created the seed file

--- db/migrate/20170313161214_add_photos_to_villas.rb
+++
@@ -1,4 +0,0 @@
-class AddPhotosToVillas < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+urls_walhahlla = [
+  'https://drive.google.com/open?id=0B4yc0IuB7OxDaXExZEY5RDFtVFU',
+  'https://drive.google.com/open?id=0B4yc0IuB7OxDbUJaZ0FnS1JqR1E',
+  'https://drive.google.com/open?id=0B4yc0IuB7OxDNU5rVHowbElWSDA',
+  'https://drive.google.com/open?id=0B4yc0IuB7OxDczlXaEVTa0l6SE0'
+]
+
+urls_tiwa = [
+  'https://drive.google.com/open?id=0B4yc0IuB7OxDR2Z0aHBUenVYYVE',
+  'https://drive.google.com/open?id=0B4yc0IuB7OxDV1ljcmhDbzJKV2M',
+  'https://drive.google.com/open?id=0B4yc0IuB7OxDeFU5LWlwWU1kcEU'
+]
+
+whalhahlla = Villa.new(name: "Whalhahlla", price: "#", address: "test address")
+whalhahlla.save!
+whalhahlla.photo_urls = urls_walhahlla
+
+tiwa = Villa.new(name: "tiwa", price: "#", address: "test address")
+tiwa.save!
+tiwa.photo_urls = urls_tiwa
