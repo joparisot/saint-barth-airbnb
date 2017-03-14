f72ade1e1069d24dd704030473674c0a5d496f44
jonathan parisot <parisot.jonathan@gmail.com>
2017-03-14 17:01:29 +0100

    created the seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314102041) do
+ActiveRecord::Schema.define(version: 20170314154343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 20170314102041) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.string   "address"
+    t.text     "description"
     t.index ["user_id"], name: "index_villas_on_user_id", using: :btree
   end
 
