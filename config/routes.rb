cd5efbc703e02e7c58ca02408a0c16f29c0eda3c
Alexandros Pantalis <alexandros.pantalis@hec.edu>
2017-03-14 12:20:49 +0100

    fb connect enabled

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
 
   get 'villas/show'
 
- devise_for :users
  root to: 'pages#home'
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  resources :villas do
    resources :bookings, only: [:new, :create]
  end
- resources :users, only: [:edit, :update, :destroy]
  mount Attachinary::Engine => "/attachinary"
+ devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+ # resources :users, only: [:edit, :update, :destroy]
 end
