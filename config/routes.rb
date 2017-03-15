bb43c611cacb3cf08a999cd254b1bb5af722edd2
emile karam <emile.karam@hec.edu>
2017-03-15 12:18:00 +0100

    fixed login bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-  get 'villas/index'
-
-  get 'villas/show'
-
  root to: 'pages#home'
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
  resources :villas do
    resources :bookings, only: [:new, :create]
  end
  post 'bookings/:id/approve', to: 'bookings#approve', as: :approve
- resources :users, only: [:edit, :update, :destroy, :show]
+ resources :users, only: [:edit, :update, :show]
  mount Attachinary::Engine => "/attachinary"
- devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
- # resources :users, only: [:edit, :update, :destroy]
-
 end
