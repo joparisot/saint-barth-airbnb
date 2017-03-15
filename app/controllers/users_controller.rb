35310178d848bdfbb21cf70e570b4b19c3218d30
Liliya Ovezmuradova <liliya.ovezmuradova@hec.edu>
2017-03-15 19:29:25 +0100

    my bookings done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,7 @@ class UsersController < ApplicationController
     @bookings_as_a_host = Booking.where(villa_id: villas_id)
     # I take bookings where villa_id belongs to all the ids from @villas (which are my villas)
   end
+
+  def edit
+  end
 end
