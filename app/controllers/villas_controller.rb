7794e6d6931cae94f59a3c9c5b5de7b52833ea7b
Alexandros Pantalis <alexandros.pantalis@hec.edu>
2017-03-16 12:16:46 +0100

    map added to show

--- app/controllers/villas_controller.rb
+++ app/controllers/villas_controller.rb
@@ -1,47 +1,52 @@
 class VillasController < ApplicationController
 skip_before_action :authenticate_user!, only: [:index, :show]
 
-def index
-  @villa = Villa.all
-  @villas = Villa.where.not(latitude: nil, longitude: nil)
+  def index
+    @villa = Villa.all
+    @villas = Villa.where.not(latitude: nil, longitude: nil)
 
-  @hash = Gmaps4rails.build_markers(@villas) do |villa, marker|
-        marker.lat villa.latitude
-        marker.lng villa.longitude
-        marker.infowindow render_to_string(partial: "/villas/map_box", locals: { villa: villa })
-      end
-end
+    @hash = Gmaps4rails.build_markers(@villas) do |villa, marker|
+          marker.lat villa.latitude
+          marker.lng villa.longitude
+          marker.infowindow render_to_string(partial: "/villas/map_box", locals: { villa: villa })
+        end
+  end
 
-def show
-  @villa = Villa.find(params[:id])
-end
+  def show
+    @villa = Villa.find(params[:id])
+    @hash = Gmaps4rails.build_markers([@villa]) do |villa, marker|
+      marker.lat villa.latitude
+      marker.lng villa.longitude
+      marker.infowindow render_to_string(partial: "/villas/map_box", locals: { villa: villa })
+    end
+  end
 
-def new
-  @villa = Villa.new
-end
+  def new
+    @villa = Villa.new
+  end
 
-def create
-  @villa = Villa.new(villa_params)
-  @villa.user = current_user
-  @villa.save
-  redirect_to villa_path(@villa)
-end
+  def create
+    @villa = Villa.new(villa_params)
+    @villa.user = current_user
+    @villa.save
+    redirect_to villa_path(@villa)
+  end
 
-def edit
-  @villa = Villa.find(params[:id])
-end
+  def edit
+    @villa = Villa.find(params[:id])
+  end
 
-def update
-  @villa = Villa.find(params[:id])
-  @villa.update(villa_params)
-  redirect_to villa_path(@villa)
-end
+  def update
+    @villa = Villa.find(params[:id])
+    @villa.update(villa_params)
+    redirect_to villa_path(@villa)
+  end
 
-private
+  private
 
-def villa_params
-  params.require(:villa).permit(:name, :address, :price, :photo)
-end
+  def villa_params
+    params.require(:villa).permit(:name, :address, :price, :photo)
+  end
 
 
 end
