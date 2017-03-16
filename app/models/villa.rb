c1f107a8c10a43df917169114627af89bba0f4aa
jonathan parisot <parisot.jonathan@gmail.com>
2017-03-16 18:52:25 +0100

    put two villas creation in seed with multiple images, deleted the photo and replaced by first of images everywhere

--- app/models/villa.rb
+++ app/models/villa.rb
@@ -5,8 +5,9 @@ class Villa < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
-  has_attachment :photo
+  # has_attachment :photo
   has_attachments :images, maximum: 30
+  validates :images, presence: true
   validates :name, presence: true
   validates :address, presence: true
   validates :price, presence: true
