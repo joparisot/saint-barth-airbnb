3891f05e09204e09b1c1df78565cd9ff62fa3131
emile karam <emile.karam@hec.edu>
2017-03-17 13:58:47 +0100

    added validations to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
   belongs_to :booking
   has_one :user, :through => :booking
   has_one :villa, :through => :booking
+
+  validates :rating, presence: true
+  validates :content, presence: true
 end
