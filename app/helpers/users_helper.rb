a8f18118140e034b9f0e447ef32177fe60f1d6d6
emile karam <emile.karam@hec.edu>
2017-03-17 13:06:54 +0100

    creater avatar helper and implemented it in reviews, profile and navbar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  #use method everytime you want to add a user picture
+  # simply write the following in your show
+  # <%= user_picture_avatar(the user you want to display, "the class you want to give the picture" %>
+  # example: <%= user_picture_avatar(current.user, "avatar-large")
+  def user_picture_avatar(user, image_class)
+   if user.photo?
+    cl_image_tag(user.photo.path, { class: image_class})
+   else
+    avatar_url = user.facebook_picture_url || "http://placehold.it/100x100"
+    image_tag avatar_url, class: image_class
+   end
+  end
 end
